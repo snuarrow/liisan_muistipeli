7f7155ee97cf47029c8b4d96378e0439d82c2b17
snuarrow <henri.vaara@helsinki.fi>
2015-09-03 15:48:26 +0300

    to 3.9.2015 15.48.26 +0300

--- target/classes/images/rename.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-i=0
-for f in * ; do
-	mv $f $i
-  let "i += 1"
-done
--- target/classes/images/resize.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for f in * ; do
-	convert $f -resize 1024x768\> $f
-	mogrify -quality 50% $f
-done
